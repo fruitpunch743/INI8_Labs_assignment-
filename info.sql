fcfa1edcad44782cb6fdfa4fd8e738baa48e6191
Meenakshi S A <107346096+fruitpunch743@users.noreply.github.com>
2024-02-10 21:35:01 +0530

    Update info.sql

--- info.sql
+++ info.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 10, 2024 at 04:56 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `info`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `registration`
---
 
 CREATE TABLE `registration` (
   `Reg_id` int(11) NOT NULL,
@@ -35,36 +12,14 @@ CREATE TABLE `registration` (
   `Date_of_registeration` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `registration`
---
-
 INSERT INTO `registration` (`Reg_id`, `Name`, `Email`, `DOB`, `Date_of_registeration`) VALUES
 (1, 'ABC', 'abc@gmail.com', '2000-04-13', '2024-02-10'),
 (2, 'DEF', 'def@gmail.com', '2003-05-07', '2024-02-10'),
 (3, 'GHI', 'ghi@gmail.com', '2001-12-08', '2024-02-10');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `registration`
---
 ALTER TABLE `registration`
   ADD PRIMARY KEY (`Reg_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `registration`
---
 ALTER TABLE `registration`
   MODIFY `Reg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
